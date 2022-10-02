ef1bc9631d29721fff91ebfac1ea6097a36db037
AmiraleBjorn <112935129+AmiraleBjorn@users.noreply.github.com>
2022-10-02 11:51:28 +0200

    life

--- ludum_dare_51/Assets/Script/Player_Life.cs
+++ ludum_dare_51/Assets/Script/Player_Life.cs
@@ -5,50 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class Player_Life : MonoBehaviour
 {
-    public int vie = 5;
-    [HideInInspector] public int vieMax;
+    public int vie = 1;
+    [HideInInspector] public int vieMax = 1;
     private Vector2 spawnPoint;
 
 
     void Start()
     {
-        if (PlayerPrefs.GetInt("playerLIFE") == 0)
-        {
-            PlayerPrefs.SetInt("playerLIFE", 5);
-        }
-        vieMax = PlayerPrefs.GetInt("playerLIFE");
         vie = vieMax;
-    }
-
-    void Update()
-    {
-        if (vie <= 0)
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        }
-    }
-
-    public void takeDamage(int damage)
-    {
-        vie = vie - damage;
-    }
-
-    public void heal(int heal)
-    {
-        vieMax = PlayerPrefs.GetInt("playerLIFE");
-        vie = vie + heal;
-        if (vie > vieMax)
-        {
-            vie = vieMax;
-        }
-    }
-
-    void OnTriggerEnter2D(Collider2D truc)
-    {
-        if (truc.tag == "checkPoint")
-        {
-            spawnPoint = truc.transform.position;
-        }
     }
     void OnCollisionEnter2D(Collision2D truc)
     {
