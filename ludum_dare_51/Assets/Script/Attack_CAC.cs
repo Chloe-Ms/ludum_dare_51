847da34e77baafad342d8aaf6d494813c2f5020c
AeroPorc <hugopv.smp@gmail.com>
2022-10-02 17:25:03 +0200

    enemy piou piou

--- ludum_dare_51/Assets/Script/Attack_CAC.cs
+++ ludum_dare_51/Assets/Script/Attack_CAC.cs
@@ -48,7 +48,7 @@ public class Attack_CAC : MonoBehaviour
             target = Physics2D.OverlapCircleAll(attackPosition, attackRadius);
             foreach (Collider2D truc in target)
             {
-                if (truc.tag == "Ennemi")
+                if (truc.tag == "Ennemy")
                 {
                     truc.SendMessage("takeDamage", degats);
                 }
