0c8e772068594ce2ba8a7e79e8c2f10a81ff4502
AeroPorc <hugopv.smp@gmail.com>
2022-10-02 17:53:10 +0200

    Brawler technically hits

--- ludum_dare_51/Assets/Scripts/AI_Brawler.cs
+++ ludum_dare_51/Assets/Scripts/AI_Brawler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class AI_Brawler : MonoBehaviour
 {
@@ -14,6 +15,15 @@ public class AI_Brawler : MonoBehaviour
     public int range;
 
     GameObject[] enemis;
+    
+    public float attackRadius;
+    public int degats = 2;
+    
+
+    public Vector2 attackPosition;
+    private Vector2 attackPositionSave;
+    private Collider2D[] target;
+    
 
 
     // Start is called before the first frame update
@@ -29,6 +39,24 @@ public class AI_Brawler : MonoBehaviour
         foreach (GameObject Player in enemis)
         {
              transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, speed * Time.deltaTime);
+            
+            attackPosition = (Vector2)transform.position + new Vector2(attackPositionSave.x, attackPositionSave.y);
+           
+
+            target = Physics2D.OverlapCircleAll(attackPosition, attackRadius);
+            foreach (Collider2D truc in target)
+            {
+                if (truc.tag == "Player")
+                {
+                    truc.SendMessage("takeDamage", degats);
+                    Debug.Log("isoké");
+                }
+            }
         }
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere((Vector2)transform.position + attackPosition, attackRadius);
     }
 }
